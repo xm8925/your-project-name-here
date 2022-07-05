bf1984d67d139a4987ebc73d9dcd8a376e0677cc
xm8925 <innocence510@gmail.com>
2022-07-05 15:48:20 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+<h1> Hello Dojo! </h1>
+
+<h3>Things I need to do:</h3>
+<ul>
+  <li>Learn React</li>
+  <li>Climb Mt. Everest</li>
+  <li>Run a marathon</li>
+  <li>Feed the dogs</li>
+</ul>
+    </>
   );
 }
 
